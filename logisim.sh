a388538541ed49a9efcfba14f3a1ea1a2507036d
Eko Julianto Salim <ekojsalim@gmail.com>
2020-09-29 15:12:22 +0700

    add logisim.sh

---
+++ logisim.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+FILE=~/Desktop/logisim.jar
+
+if type -p java; then
+    echo "Found java executable in PATH"
+else
+    echo "Please install Java first :)"
+fi
+
+if test -f "$FILE"; then
+    echo "$FILE exists. Running from that"
+    java -jar $FILE
+else
+    echo "$FILE doesn't exists. Downloading"
+    curl -o ~/Desktop/logisim.jar  http://ekojs.com/logisim.jar
+    java -jar $FILE
+fi
